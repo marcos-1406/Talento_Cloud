ed078b996961e618d7b87d69ea2cc67fa3788d2c
marcos-1406 <83986578+marcos-1406@users.noreply.github.com>
2023-12-05 16:26:26 -0300

    Create P_HT_Desenvolvimento_1.py

---
+++ P_HT_Desenvolvimento_1.py
@@ -0,0 +1,8 @@
+Produtos = ["Pão", "Leite", "Manteiga", "Biscoito", "Café"]
+Anos_Nascimento = [1974, 1979, 2000, 2001, 2003]
+
+for i in range(len(Produtos)):
+  print("Produto " + str(i+1) + " : " + str(Produtos[i]))
+
+for i in range(len(Anos_Nascimento)):
+  print("Ano " + str(i+1) + " : " + str(Anos_Nascimento[i]))
